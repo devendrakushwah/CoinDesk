31ef6c194b45500ff20d69c17682f499459ecc39
Devendra Kushwah <idevendra08@gmail.com>
2019-02-15 20:51:46 +0530

    added currencies

--- app/src/main/java/com/readybrains/coindesk/activities/SettingsActivity.java
+++ app/src/main/java/com/readybrains/coindesk/activities/SettingsActivity.java
@@ -1,69 +1,42 @@
 package com.readybrains.coindesk.activities;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
-
 import com.readybrains.coindesk.R;
 
-import java.util.ArrayList;
-import java.util.Currency;
-import java.util.Set;
-import java.util.prefs.Preferences;
-
 public class SettingsActivity extends AppCompatActivity {
-
     Spinner sp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         sp=findViewById(R.id.settings_dropdown);
-
-
         SharedPreferences sharedPreferences=PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         final SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        Set currencySet=Currency.getAvailableCurrencies();
-        ArrayList<String> currencyAdapter =new ArrayList<String>();
-        for(Object c:currencySet){
-            currencyAdapter.add("    "+c.toString()+"    ");
-        }
-
+        String currencyAdapter[]={"AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTC","BTN","BWP","BYN","BYR","BZD","CAD","CDF","CHF","CLF","CLP","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LTL","LVL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VEF","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPF","YER","ZAR","ZMK","ZMW","ZWL"};
         String current = sharedPreferences.getString("currency","USD");
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, currencyAdapter);
-        int pos=adapter.getPosition("    "+current+"    ");
-
+        int pos=adapter.getPosition(current);
         sp.setAdapter(adapter);
         sp.setSelection(pos);
-
         sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
                 String selectedItem=parent.getItemAtPosition(position).toString();
-                editor.putString("currency",selectedItem.substring(4,7));
+                editor.putString("currency",selectedItem);
                 editor.commit();
-
-                Snackbar.make(parent,"Default currecncy changed to "+selectedItem.substring(4,7),Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(parent,"Default currency changed to "+selectedItem,Snackbar.LENGTH_SHORT).show();
             }
             public void onNothingSelected(AdapterView<?> parent)
             {
-
             }
         });
-
-
     }
 }
--- app/src/main/java/com/readybrains/coindesk/activities/SplashActivity.java
+++ app/src/main/java/com/readybrains/coindesk/activities/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.readybrains.coindesk.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -7,13 +8,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.readybrains.coindesk.R;
+import com.readybrains.coindesk.adapters.CoinsAdapter;
+import com.readybrains.coindesk.models.Coins;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SplashActivity extends AppCompatActivity {
 
     RelativeLayout relativeLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
